953ac7a83dbcf2b16e6fd75e2e91bacb807da552
Vadim Galaktionov <vadim.galaktionov@gmail.com>
2016-06-09 15:51:27 +0200

    added readme and shakespeare

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# data-structures-python
+This repo contains my solutions to the problems in the book "Problem Solving with Algorithms and Data Structures, Release 3.0" by Brad Miller and David Ranum.(https://www.cs.auckland.ac.nz/courses/compsci105ssc/resources/ProblemSolvingwithAlgorithmsandDataStructures.pdf)
+
+The goal for the solutions is to be readable and pythonic, and as efficient as I can make them.
--- shakespeare_challenge.py
+++ shakespeare_challenge.py
@@ -1,18 +1,27 @@
 import random
 
-target_string = 'methinks it is like a weasel'
 alphabet = 'abcdefghijklmnopqrstuvwxyz '
+target_string = 'methinks it is like a weasel'
 
 
-def monkey_typewriter():
+def monkey_initial():
     sentence = ''
     for i in range(0, 28):
         sentence += alphabet[random.randint(0, 26)]
     return sentence
 
 
+def monkey_improve(sentence):
+    sentence = list(sentence)
+    for index, letter in enumerate(sentence):
+        if letter != target_string[index]:
+            sentence[index] = alphabet[random.randint(0, 26)]
+            break
+    sentence = ''.join(sentence)
+    return sentence
+
+
 def scorer(sentence):
-    target_string = 'methinks it is like a weasel'
     score = 0
     for index, letter in enumerate(sentence):
         if letter == target_string[index]:
@@ -21,18 +30,26 @@ def scorer(sentence):
 
 
 def looper():
+
     attempts = 0
     high_score = 0
+    sentence = monkey_initial()
+    best_attempt = ""
     while True:
-        sentence = monkey_typewriter()
         score = scorer(sentence)
-        attempts += 1
-        if score == 27:
+
+        if score == 28:
             print('Success after {} attempts'.format(attempts))
             break
+
         if score > high_score:
             high_score = score
             best_attempt = sentence
+
+        attempts += 1
+
         if attempts % 1000 == 0:
             print('Best attempt : "{}", score: {}'.format(
                 best_attempt, high_score))
+
+        sentence = monkey_improve(sentence)
