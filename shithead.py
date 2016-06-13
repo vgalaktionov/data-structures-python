f59db358fad57d942d2d466293699fae6ef3654a
Vadim Galaktionov <vadim.galaktionov@gmail.com>
2016-06-13 09:23:28 +0200

    200% less buggy

--- shithead.py
+++ shithead.py
@@ -1,4 +1,3 @@
-
 """
 This script implements a simplistic 2 player version of the card game 'Shithead'. It is simplistic
 in that the computer opponent always plays a random card.
@@ -55,6 +54,20 @@ class CardDeck:
     def get_card_count(self):
         return len(self.cards)
 
+    def get_player_card_count(self):
+        if self.get_card_count() > 30:
+            print('Deck cards: over 30')
+        elif self.get_card_count() > 20:
+            print('Deck cards: over 20')
+        elif self.get_card_count() > 10:
+            print('Deck cards: over 10')
+        elif self.get_card_count() > 5:
+            print('Deck cards: over 5')
+        elif self.get_card_count() > 0:
+            print('Deck cards: under 5')
+        else:
+            print('Deck empty!')
+
 
 class Player:
 
@@ -101,6 +114,42 @@ class Player:
                     if card.value == to_remove:
                         self.hand.pop(card_index)
 
+    def check_play_duplicates(self, last_card, computer=False):
+        hand_values = [card.value for card in self.hand]
+        if hand_values.count(last_card.value) > 0:
+            if computer or input('Play duplicates of {}? [y/n] '.format(str(last_card))) == 'y':
+                for card_index, card in enumerate(self.hand):
+                    if card.value == last_card.value:
+                        self.play_card(card_index, stack)
+                        self.check_play_duplicates(last_card, computer=True)
+
+    def pick_check_valid(self, which_cards):
+        if which_cards == human.hidden_cards:
+            which_cards_str = 'Hidden'
+        elif which_cards == human.open_cards:
+            which_cards_str = 'Open'
+        else:
+            which_cards_str = 'Hand'
+        card_to_play = int(input('{} card to play? '.format(which_cards_str) +
+                                 str([x for x in range(len(which_cards) + 1)]) + ' ')) - 1
+        if card_to_play not in range(-1, len(which_cards)):
+            print('Sorry, please pick a card number between 0 and {}'.format(len(which_cards)))
+            card_to_play = self.pick_check_valid(which_cards)
+        return card_to_play
+
+    def take_stack(self, open_cards=False, computer=False):
+        global stack
+        if computer:
+            print('Opponent took the stack.')
+        else:
+            print('You took the stack.')
+        if open_cards:
+            self.open_cards.extend(stack[-1])
+            self.hand.extend(stack[0:-1])
+        else:
+            self.hand.extend(stack)
+        stack = []
+
 
 def print_table():
 
@@ -111,6 +160,7 @@ def print_table():
 
     print('_______________________________________\n' + str(human) +
           '\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
 
 # Main game logic starts here
 
@@ -143,95 +193,80 @@ while player_wants_to_switch == 'y':
 
     human.switch_cards(open_card_to_switch, hand_card_to_switch)
     print(human)
-    player_wants_to_switch = input('Switch cards? [y/n] ')
+    player_wants_to_switch = input('Switch cards? [y/n]')
 
 opponent.play_card(random.randint(0, len(opponent.hand) - 1), stack)
 
 while human.count_cards() > 0 and opponent.count_cards() > 0:
 
-    print('You are up: ')
+    print('\n')
+    deck.get_player_card_count()
+    print('\nYou are up: ')
+
     human.check_remove_4()
 
     if len(human.hand) > 0:
         while len(human.hand) < 3 and len(deck.cards) > 0:
             deck.deal(human)
         print_table()
-        card_to_play = int(input('Hand card to play? ' +
-                                 str([x for x in range(len(human.hand) + 1)]) + ' ')) - 1
+        card_to_play = human.pick_check_valid(human.hand)
         if card_to_play >= 0:
             human.play_card(card_to_play, stack)
             if len(stack) > 1 and stack[-1].value < stack[-2].value:
-                print('You took the stack.')
-                human.hand.extend(stack)
-                stack = []
+                human.take_stack()
+            else:
+                human.check_play_duplicates(stack[-1])
         else:
-            print('You took the stack.')
-            human.hand.extend(stack)
-            stack = []
+            human.take_stack()
 
     elif len(human.open_cards) > 0:
         print_table()
-        card_to_play = int(input('Open card to play? ' +
-                                 str([x for x in range(len(human.open_cards) + 1)]) + ' ')) - 1
+        card_to_play = human.pick_check_valid(human.open_cards)
         if card_to_play >= 0:
             human.play_card(card_to_play, stack, open_cards=True)
             if len(stack) > 1 and stack[-1].value < stack[-2].value:
-                print('You took the stack.')
-                human.open_cards.extend(stack[-1])
-                human.hand.extend(stack[0:-1])
-                stack = []
+                human.take_stack(open_cards=True)
         else:
-            print('You took the stack.')
-            human.hand.extend(stack)
-            stack = []
+            human.take_stack()
 
     else:
         print_table()
-        card_to_play = int(input('Hidden card to play? ' +
-                                 str([x for x in range(len(human.hidden_cards) + 1)]) + ' ')) - 1
+        card_to_play = human.pick_check_valid(human.hidden_cards)
         if card_to_play >= 0:
             human.play_card(card_to_play, stack, hidden_cards=True)
             if len(stack) > 1 and stack[-1].value < stack[-2].value:
-                print('You took the stack.')
-                human.hand.extend(stack)
-                stack = []
+                human.take_stack()
         else:
-            print('You took the stack.')
-            human.hand.extend(stack)
-            stack = []
+            human.take_stack()
 
-    print_table()
-    print('Opponent is up: ')
+    print('\nOpponent is up: ')
     opponent.check_remove_4()
 
     if len(opponent.hand) > 0:
+        print_table()
         while len(opponent.hand) < 3 and len(deck.cards) > 0:
             deck.deal(opponent)
         opponent.play_card(random.randint(0, len(opponent.hand) - 1), stack)
         if len(stack) > 1 and stack[-1].value < stack[-2].value:
-            print('Opponent took the stack.')
-            opponent.hand.extend(stack)
-            stack = []
+            opponent.take_stack(computer=True)
+        else:
+            opponent.check_play_duplicates(stack[-1], computer=True)
 
     elif len(opponent.open_cards) > 0:
         print_table()
-        opponent.play_card(random.randint(0, len(opponent.open_cards) - 1), stack)
+        opponent.play_card(random.randint(0, len(opponent.open_cards) - 1), stack, open_cards=True)
         if len(stack) > 1 and stack[-1].value < stack[-2].value:
-            print('Opponent took the stack.')
-            opponent.open_cards.extend(stack[-1])
-            opponent.hand.extend(stack[0:-1])
-            stack = []
+            opponent.take_stack(open_cards=True, computer=True)
 
     else:
         print_table()
-        opponent.play_card(random.randint(0, len(opponent.hidden_cards) - 1), stack)
+        opponent.play_card(random.randint(0, len(opponent.hidden_cards) - 1),
+                           stack, hidden_cards=True)
         if len(stack) > 1 and stack[-1].value < stack[-2].value:
-            print('Opponent took the stack.')
-            opponent.hand.extend(stack)
-            stack = []
+            opponent.take_stack(computer=True)
 
 
 if human.count_cards() == 0:
-    print('You win!')
+    print('\nYou win!\n')
 else:
-    print('Computer wins!')
+    print('\nComputer wins!\n')
